14aa65ba976cc5426f627d6d0f079e2262d87446
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-01-25 17:58:03 +0000

    Fixed indentation. Added user/group for each os.

--- test/integration/wordpress_config/serverspec/wordpress_config_spec.rb
+++ test/integration/wordpress_config/serverspec/wordpress_config_spec.rb
@@ -5,16 +5,44 @@ set :backend, :exec
 
 describe 'wordpress configuration' do
 
-    configs = ['/var/html/sitenameA.com/wp-config.php', '/var/html/sitenameB.com/wp-config.php']
-
-    configs.each do |config|
-        describe file(config) do
-            it { should exist }
-            it { should be_file }
-            it { should be_mode 644 }
-            it { should be_owned_by 'www-data' }
-            it { should be_grouped_into 'www-data' }
-            it { should contain "dbuser" }
-        end
+  case os[:family]
+  when 'debian'
+    case os[:release]
+    when '7.11'
+      user = 'www-data'
+      group = 'www-data'
+    when '8.6'
+      user = 'www-data'
+      group = 'www-data'
     end
+  when 'redhat'
+    user = 'apache'
+    group = 'apache'
+  when 'ubuntu'
+    case os[:release]
+    when '14.04'
+      user = 'www-data'
+      group = 'www-data'
+    when '16.04'
+      user = 'www-data'
+      group = 'www-data'
+    end
+  end
+
+  configs = ['/var/html/sitenameA.com/wp-config.php', '/var/html/sitenameB.com/wp-config.php']
+
+  configs.each do |config|
+    describe file(config) do
+      it { should exist }
+      it { should be_file }
+      it { should be_mode 644 }
+      it { should be_owned_by user }
+      it { should be_grouped_into group }
+      it { should contain "localhost" }
+      it { should contain "dbuser" }
+      it { should contain "dbpass" }
+      it { should contain "@email.com" }
+    end
+  end
+
 end
