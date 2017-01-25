a35b1750cdefd02ab9b8431490b90da87d8d6a8f
Fintan MacMahon <fintan.macmahon@gmail.com>
2017-01-25 18:01:13 +0000

    Fixed indentation. Added user/group for each os.

--- test/integration/wordpress_cli/serverspec/wordpress_cli_spec.rb
+++ test/integration/wordpress_cli/serverspec/wordpress_cli_spec.rb
@@ -4,13 +4,36 @@ require "serverspec"
 set :backend, :exec
 
 describe 'wordpress cli tool' do
-
-    wp_cli = '/usr/local/bin/wp'
-
-    describe file(wp_cli) do
-        it { should exist }
-        it { should be_mode 740 }
-        it { should be_owned_by 'www-data' }
-        it { should be_grouped_into 'www-data' }
+  case os[:family]
+  when 'debian'
+    case os[:release]
+    when '7.11'
+      user = 'www-data'
+      group = 'www-data'
+    when '8.6'
+      user = 'www-data'
+      group = 'www-data'
     end
+  when 'redhat'
+    user = 'apache'
+    group = 'apache'
+  when 'ubuntu'
+    case os[:release]
+    when '14.04'
+      user = 'www-data'
+      group = 'www-data'
+    when '16.04'
+      user = 'www-data'
+      group = 'www-data'
+    end
+  end
+
+  wp_cli = '/usr/local/bin/wp'
+
+  describe file(wp_cli) do
+    it { should exist }
+    it { should be_mode 740 }
+    it { should be_owned_by user }
+    it { should be_grouped_into group }
+  end
 end
